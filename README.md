d6ff311a6ff6d1f6e2173b4002e8f75d34f9c9d0
John Zhou <john.zhou1@ucalgary.ca>
2025-11-19 01:18:21 -0700

    .env

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ENSF614-Project
+# You will need something like the following in the .env in root folder
+DB_URL=jdbc:mysql://localhost:3306/flightdb?serverTimezone=UTC  
+DB_USER=root  
+DB_PASS=john222  
--- src/main/java/flightapp/data/DBConnection.java
+++ src/main/java/flightapp/data/DBConnection.java
@@ -1,15 +1,20 @@
 package flightapp.data;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
 
-    // TODO: adjust DB name / user / password
-    private static final String URL  = "jdbc:mysql://localhost:3306/flightdb?serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASS = "john222";
+    private static final Dotenv dotenv = Dotenv.configure()
+            .directory("./")     // look in project root
+            .ignoreIfMissing()   // prevents crash if no .env (optional)
+            .load();
+
+    private static final String URL  = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASS = dotenv.get("DB_PASS");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
